4f1ae088ef993f3280a7a4f8bcfd6393faa27290
Olivier Grisel <olivier.grisel@ensta.org>
2014-07-31 09:26:38 +0200

    Flatter docker image by cutting RUN steps

--- Dockerfile
+++ Dockerfile
@@ -3,27 +3,6 @@
 FROM ubuntu:14.04
 MAINTAINER Olivier Grisel <olivier.grisel@ensta.org>
 
-RUN mkdir /tmp/build
-WORKDIR /tmp/build
-RUN locale-gen en_US en_US.UTF-8
-
-RUN apt-get -y update
-RUN apt-get -y install git-core build-essential gfortran
-
-# Build latest stable release from OpenBLAS from source
-RUN git clone -q --branch=master git://github.com/xianyi/OpenBLAS.git
-RUN (cd OpenBLAS \
-     && make DYNAMIC_ARCH=1 NO_AFFINITY=1 NUM_THREADS=32 \
-     && make install)
 ADD openblas.conf /etc/ld.so.conf.d/openblas.conf
-RUN ldconfig
-
-# Restore default workdir
-WORKDIR /
-
-# Cleanup build artifacts and dependencies to minimize image size
-RUN rm -rf /tmp/build
-
-RUN (apt-get remove -y --purge git-core build-essential; \
-     apt-get autoremove -y; \
-     apt-get clean -y)
+ADD build_openblas.sh build_openblas.sh
+RUN bash build_openblas.sh
---
+++ build_openblas.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+# Build OpenBLAS and clean up build dependencies to have put everything in a
+# single RUN step and workaround for:
+# https://github.com/docker/docker/issues/332
+
+set -xe
+
+mkdir /tmp/build
+cd /tmp/build
+
+apt-get -y update
+apt-get -y install git-core build-essential gfortran
+
+# Build latest stable release from OpenBLAS from source
+git clone -q --branch=master git://github.com/xianyi/OpenBLAS.git
+(cd OpenBLAS \
+    && make DYNAMIC_ARCH=1 NO_AFFINITY=1 NUM_THREADS=32 \
+    && make install)
+
+# Rebuild ld cache, this assumes that:
+# /etc/ld.so.conf.d/openblas.conf was installed by Dockerfile
+# and that the libraries are in /opt/OpenBLAS/lib
+ldconfig
+
+#Minimize image size (gfortran is needed at runtime)
+apt-get remove -y --purge git-core build-essential
+apt-get autoremove -y
+apt-get clean -y
+
+cd /
+rm -rf /tmp/build
+rm -rf /build_openblas.sh
